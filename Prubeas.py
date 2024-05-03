a638640711e6eccd6bdb46932eeeb3cd6775b628
Sebasmindi2122 <132713164+Sebasmindi2122@users.noreply.github.com>
2024-05-03 00:38:31 +0000

    Thu, May 2, 2024, 7:38 PM -05:00

--- Prubeas.py
+++ Prubeas.py
@@ -3,11 +3,26 @@ import sys
 
 from util import Node, StackFrontier, QueueFrontier
 
+directory = "/home/ubuntu/Conexi-n-de-actores/small"
 # Maps names to a set of corresponding person_ids
 names = {}
 
 # Maps person_ids to a dictionary of: name, birth, movies (a set of movie_ids)
+
 people = {}
 
 # Maps movie_ids to a dictionary of: title, year, stars (a set of person_ids)
 movies = {}
+# Load people
+with open(f"{directory}/people.csv", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        people[row["id"]] = {
+            "name": row["name"],
+            "birth": row["birth"],
+            "movies": set()
+        }
+        if row["name"].lower() not in names:
+            names[row["name"].lower()] = {row["id"]}
+        else:
+            names[row["name"].lower()].add(row["id"])
