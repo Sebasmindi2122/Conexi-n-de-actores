362af2039cbfb55ec9b5e114ac8ba1f6d636ed4a
Sebasmindi2122 <132713164+Sebasmindi2122@users.noreply.github.com>
2024-05-03 00:43:10 +0000

    Thu, May 2, 2024, 7:43 PM -05:00

--- Prubeas.py
+++ Prubeas.py
@@ -26,6 +26,23 @@ with open(f"{directory}/people.csv", encoding="utf-8") as f:
             names[row["name"].lower()] = {row["id"]}
         else:
             names[row["name"].lower()].add(row["id"])
+# Load movies
+with open(f"{directory}/movies.csv", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        movies[row["id"]] = {
+            "title": row["title"],
+            "year": row["year"],
+            "stars": set()
+        }
 
-print(names)
+# Load stars
+with open(f"{directory}/stars.csv", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        try:
+            people[row["person_id"]]["movies"].add(row["movie_id"])
+            movies[row["movie_id"]]["stars"].add(row["person_id"])
+        except KeyError:
+            pass
 
